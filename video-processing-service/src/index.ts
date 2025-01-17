e6170c3c8a20979b562882a1895db4b081cf7f4a
kkig <ex@sample.com>
2025-01-17 13:35:45 +0900

    Add video processing service

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -2,12 +2,34 @@ import express from "express";
 import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hell World!");
+    res.send("Welcome to Video Processing Service.");
 })
 
+app.post("/process-video", (req, res) => {
+    // Get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing file path.");
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") // 360p
+        .on("end", () => {
+            res.status(200).send("Processing finished successfully.");
+        })
+        .on("error", (err) => {
+            console.log(`Error occurred: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`);
+        })
+        .save(outputFilePath);
+})
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Video processing service is running on port http://localhost:${port}`);
 });
