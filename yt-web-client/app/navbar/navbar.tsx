b3834033e0fcc00cfc1f1707f5cebfb6e6058ca9
kkig <ex@sample.com>
2025-01-22 23:06:30 +0900

    Add sign in with Google

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,15 +1,37 @@
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
 
-import styles from './navbar.module.css';
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./navbar.module.css";
+import SignIn from "./sign-in"
+import { onAuthStateChangedHelper } from "../utilities/firebase/firebase";
+import { useState, useEffect } from "react";
+import { User } from "firebase/auth";
 
 export default function Navbar() {
+    // Init user state
+    const [user, setUser] = useState<User | null>(null);
+
+     useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+
+        // Cleanup subscription on unmount
+        return () => unsubscribe();
+     });
+
     return (
         <nav className={styles.nav}>
             <Link href='/' >
                 <Image width={90} height={20}
                     src='/youtube-logo.svg' alt='YouTube Logo' />
             </Link>
+            {
+                // TODO: Add an upload button
+            }
+            <SignIn user={user} />
         </nav>
     );
-}
+};
