33fb13363faffc109dde5eed269ba3be0168aa6b
kkig <ex@sample.com>
2025-01-24 22:52:21 +0900

    Add getVideos function

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -13,6 +13,8 @@ const storage = new Storage();
 
 const rawVideoBucketName = "nc-yt777-raw-videos";
 
+const videoCollectionId = "videos";
+
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
     uid: user.uid,
@@ -50,4 +52,11 @@ export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
   });
 
   return {url, fileName};
+});
+
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const snapshot =
+    await firestore.collection(videoCollectionId).limit(10).get();
+  return snapshot.docs.map((doc) => doc.data());
 });
